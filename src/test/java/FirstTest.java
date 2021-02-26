fe7fc94ee90c7b82a15ed67cddbd3821b846b4ad
Armen <amo.777@mail.ru>
2021-02-26 16:57:15 +0400

    Task 4

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.*;
+import org.testng.asserts.SoftAssert;
 
 public class FirstTest {
 
@@ -10,6 +11,7 @@ public class FirstTest {
     @AfterAll
     public static void endedClassTests() {
         System.out.printf("All tests in %s finished%n", FirstTest.class.getName());
+
     }
 
     @BeforeEach
@@ -24,32 +26,53 @@ public class FirstTest {
 
     @Test
     public void myTest() {
+        SoftAssert softAssert = new SoftAssert();
         System.out.println("My first autotest running");
+        softAssert.assertEquals(1/0, 1);
+        softAssert.assertAll();
     }
 
     @Test
     public void test1() {
+        SoftAssert softAssert = new SoftAssert();
         System.out.println("Test №1");
+        softAssert.assertEquals(2+2, 4);
+        softAssert.assertAll();
     }
 
     @Test
     public void test2() {
+        SoftAssert softAssert = new SoftAssert();
         System.out.println("Test №2");
+        softAssert.assertEquals(2+2, 5);
+        softAssert.assertAll();
     }
 
     @Test
     public void test3() {
+        SoftAssert softAssert = new SoftAssert();
         System.out.println("Test №3");
+        softAssert.assertTrue(2+2==4);
+        softAssert.assertAll();
     }
 
     @Test
     public void test4() {
+        SoftAssert softAssert = new SoftAssert();
         System.out.println("Test №4");
+        softAssert.assertTrue(2+2==5);
+        softAssert.assertAll();
     }
 
     @Test
     public void test5() {
+        SoftAssert softAssert = new SoftAssert();
         System.out.println("Test №5");
+        softAssert.assertEquals(2+2, 4);
+        softAssert.assertEquals(2+2, 5);
+        softAssert.assertTrue(2+2==4);
+        softAssert.assertTrue(2+2==5);
+        softAssert.assertAll();
     }
 
 }
